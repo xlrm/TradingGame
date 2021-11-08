7e6365095e92a22b7449492ca7826a71fe6a1239
xlrm <76971125+xlrm@users.noreply.github.com>
2021-11-09 00:07:02 +0900

    버튼 클릭 인식
    게임시작, 게임방법 버튼은 임시로 만듦.
    버튼 class 파일을 button.py파일로 따로 해두고 TradingGame.py 파일에서 import button을 통해 가져옴

--- TradingGame.py
+++ TradingGame.py
@@ -1,33 +1,39 @@
 import pygame as pg
 import matplotlib.pyplot as plt
 import numpy as np
+import button, sys
 
 pg.init()
-
-BLACK= ( 0,  0,  0)
 WHITE= (255,255,255)
- 
 size  = [1500,800]
 screen= pg.display.set_mode(size)
-  
-pg.display.set_caption("주식 게임")
-  
-done= False
-clock= pg.time.Clock()
+clock = pg.time.Clock()
 
-while not done: 
-  
+pg.display.set_caption("주식 게임")
+
+start_img = pg.image.load('게임시작.png')
+howto_img = pg.image.load('게임방법.png')
+
+
+
+start_button = button.Button(650, 500, start_img, 1)
+howto_button = button.Button(650, 600, howto_img, 1)
+
+
+run = True
+while run:
     clock.tick(60)
-     
     for event in pg.event.get():
         if event.type == pg.QUIT:
-            done=True
-
+            run = False
+            
     screen.fill(WHITE)
+
+    if start_button.draw(screen) == True:
+        print('GAME START')
+    if howto_button.draw(screen) == True:
+        print('HOW TO PLAY THE GAME')
  
     pg.display.flip()
 
-
-
 pg.quit()
-#양준서
