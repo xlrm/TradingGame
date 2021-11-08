7f6c14ec504a41412a7775a0fa8bb4a4ebab7455
xlrm <76971125+xlrm@users.noreply.github.com>
2021-11-09 00:19:14 +0900

    Update TradingGame.py
    게임 시작 버튼을 누르면 게임이 꺼짐. (36번줄 run = False)
    나중에는 그 자리(else 밑 블록)에 게임 화면 코드가 들어감.

--- TradingGame.py
+++ TradingGame.py
@@ -14,7 +14,7 @@ pg.display.set_caption("주식 게임")
 start_img = pg.image.load('게임시작.png')
 howto_img = pg.image.load('게임방법.png')
 
-
+main_menu = True
 
 start_button = button.Button(650, 500, start_img, 1)
 howto_button = button.Button(650, 600, howto_img, 1)
@@ -23,16 +23,22 @@ howto_button = button.Button(650, 600, howto_img, 1)
 run = True
 while run:
     clock.tick(60)
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            run = False
             
     screen.fill(WHITE)
 
-    if start_button.draw(screen) == True:
-        print('GAME START')
-    if howto_button.draw(screen) == True:
-        print('HOW TO PLAY THE GAME')
+    if main_menu == True:
+        if start_button.draw(screen) == True:
+            print('GAME START')
+            main_menu = False
+        if howto_button.draw(screen) == True:
+            print('HOW TO PLAY THE GAME')
+    else:
+        run = False
+        
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            run = False
  
     pg.display.flip()
 
